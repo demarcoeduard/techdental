faf4c852fd7f6f7b91f5b699a7b0be1fae7125f4
demarcoeduard <edydumy@gmail.com>
2024-06-16 19:57:21 +0300

    made images path relative

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,21 @@ export class AppComponent {
   features = [
     {
       title: "Advanced Technology",
-      image: "../assets/feature-1.png",
+      image: "assets/feature-1.png",
       point1: "Precise Tools: Ensures a perfect fit with 3D scanning and CNC milling.",
       point2: "Fast Turnaround: Reduces wait times with rapid production.",
       point3: "High Quality: Uses cutting-edge materials for better results."
     },
     {
       title: "Expertise & Experience",
-      image: "../assets/feature-2.png",
+      image: "assets/feature-2.png",
       point1: "Specialized Knowledge: Experts in various dental prosthetics fields.",
       point2: "Extensive Experience: Workers with over 10 years of expertise.",
       point3: "Continuous Education: Regularly updated through courses."
     },
     {
       title: "High-Quality Materials",
-      image: "../assets/feature-3.png",
+      image: "assets/feature-3.png",
       point1: "Long-Lasting: Superior materials that ensure the most best results.",
       point2: "Aesthetic Appeal: Advanced materials provide a natural look.",
       point3: "Biocompatible: Materials are safe and tolerated by the body."
